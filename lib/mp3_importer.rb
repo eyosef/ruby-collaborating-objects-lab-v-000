65f57b46ce679f9f88ed3fed4b01658589df36a4
Eden Mesfun <emesfun@gmail.com>
2018-02-16 20:14:41 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,6 @@
 class MP3Importer
 
-  attr_accessor :path
+  attr_accessor :path, :filenames
 
   def initialize(path)
     @path=path
@@ -12,10 +12,10 @@ class MP3Importer
   #MP3Importer.new('./db/mp3s').import
   # "/home/eyosef/ruby-collaborating-objects-lab-v-000" (directory file this is currently in)
   def files
-    current_dir = Dir.getwd
-    Dir.chdir(@path)
-    filenames = Dir.glob("*.mp3")
-    Dir.chdir(current_dir)
+    current_dir = Dir.getwd #name of current directory
+    Dir.chdir(@path) #change directory to instance of path
+    filenames = Dir.glob("*.mp3") #setting variable equal to mp3s in @path
+    Dir.chdir(current_dir) #
     filenames
   end
 
